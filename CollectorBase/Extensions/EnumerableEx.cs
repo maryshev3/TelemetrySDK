30d9480cc906a1c141a29dfa4c2d814ae232d9c2
Марышев Андрей Владимирович <maryshev_av@rtsoft.ru>
2024-08-12 11:32:16 +0300

    Доработал построение дерева

--- CollectorBase/Extensions/EnumerableEx.cs
+++ CollectorBase/Extensions/EnumerableEx.cs
@@ -15,52 +15,22 @@ public static class EnumerableEx
     /// <returns>Древовидный список телеметрии</returns>
     public static IEnumerable<TelemetryItem> CreateTree(this IEnumerable<TelemetryItem> telemetryItems)
     {
-        IEnumerable<TelemetryItem> telemetryItemsCopy = telemetryItems.CreateDeepCopy();
+        TelemetryItem[] telemetryItemsCopy = telemetryItems.CreateDeepCopy().ToArray();
+        
+        // Словарь ParentId -> Элементы, у которых одинаковый ParentId.
+        var parentIdToItems = telemetryItemsCopy.ToLookup(x => x.ParentId);
 
-        (Dictionary<string, List<string>> idToChildrens, Dictionary<string, TelemetryItem> idToTelemetryItems, HashSet<string> childrenIds) mapsOfTelemetries = telemetryItemsCopy
-            .Aggregate(
-                (new Dictionary<string, List<string>>(), new Dictionary<string, TelemetryItem>(), new HashSet<string>()),
-                (maps, telemetryItem) =>
-                {
-                    // Пополняем словарь ид -> ид детей
-                    if (!maps.Item1.ContainsKey(telemetryItem.Id))
-                    {
-                        maps.Item1.Add(telemetryItem.Id, new List<string>());
-                    }
-                    
-                    if (maps.Item1.ContainsKey(telemetryItem.ParentId))
-                    {
-                        maps.Item1[telemetryItem.ParentId].Add(telemetryItem.Id);
-                        
-                        // Это не корневой элемент. Добавляем его в список "детей".
-                        maps.Item3.Add(telemetryItem.Id);
-                    }
-                    else
-                    {
-                        maps.Item1.Add(telemetryItem.ParentId, new List<string>());
-                    }
-                    
-                    // Пополняем словарь ид -> объект телеметрии
-                    maps.Item2.Add(telemetryItem.Id, telemetryItem);
-                    
-                    return (maps.Item1, maps.Item2, maps.Item3);
-                }
-            );
+        // Заполняем "детей".
+        foreach (var item in telemetryItemsCopy)
+        {
+            item.Childrens = parentIdToItems[item.Id].ToList();
+        }
+        
+        // Возвращаем только корневые элементы.
+        // Множество Id.
+        var ids = telemetryItemsCopy.Select(x => x.Id).ToHashSet();
 
-        return mapsOfTelemetries
-            .idToChildrens
-            .Where(x => 
-                mapsOfTelemetries.idToTelemetryItems.ContainsKey(x.Key)
-                && !mapsOfTelemetries.childrenIds.Contains(x.Key)
-            )
-            .Select(x =>
-            {
-                TelemetryItem parent = mapsOfTelemetries.idToTelemetryItems[x.Key];
-
-                parent.Childrens = x.Value.Select(y => mapsOfTelemetries.idToTelemetryItems[y]).ToList();
-
-                return parent;
-            }
-        );
+        // Список "корневых" элементов.
+        return telemetryItemsCopy.Where(x => !ids.Contains(x.ParentId));
     }
 }
